4c8d273f6967494bc44256d1fcb92919b6c9b286
Alex-A4 <32712725+Alex-A4@users.noreply.github.com>
2020-03-22 11:49:26 +0300

    adding new layouts

--- app/src/main/java/com/alkam/passwordgenerator/AddRecords.kt
+++ app/src/main/java/com/alkam/passwordgenerator/AddRecords.kt
@@ -3,11 +3,11 @@ package com.alkam.passwordgenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AddRecords : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.main_toolbar))
+        setContentView(R.layout.activity_add_records)
+        setSupportActionBar(findViewById(R.id.add_record_toolbar))
     }
 }
